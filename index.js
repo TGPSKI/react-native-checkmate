59e0086dcbb6284016d10684ad02cef5ad70ce7d
Tyler Pate <tyler@21.co>
2018-07-12 12:28:27 -0700

    index: add module.exports for all sub-modules

---
+++ index.js
@@ -0,0 +1,15 @@
+const general = require('./general.js');
+const versions = require('./versions.js');
+const node = require('./node.js');
+const ios = require('./ios.js');
+const android = require('./android.js');
+const env = require('./env.js');
+
+module.exports = {
+  general: general,
+  versions: versions,
+  node: node,
+  ios: ios,
+  android: android,
+  env: env
+};
