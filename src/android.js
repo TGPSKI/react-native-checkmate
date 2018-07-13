20b4eb607edb951171b7bd77d9c06f4f2daea164
Tyler Pate <tyler@21.co>
2018-07-13 11:37:48 -0700

    android: remove cwd argument

--- src/android.js
+++ src/android.js
@@ -1,7 +1,9 @@
 const shell = require('shelljs');
 
+const cwd = process.cwd().concat('/');
+
 // Check gradle tasks for execution ability
-function gradleTasksCheck(cwd) {
+function gradleTasksCheck() {
   shell.cd(cwd.concat('android'));
   let out = shell.exec('./gradlew tasks').code;
   shell.cd(cwd);
